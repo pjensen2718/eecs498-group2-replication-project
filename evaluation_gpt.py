112fbb23e9df37354d2c41b6dea739d593e9dd75
Parker Jensen <pjensen2718@gmail.com>
2024-11-05 23:27:30 -0500

    begin learning about OpenAI API

--- evaluation_gpt.py
+++ evaluation_gpt.py
@@ -1,10 +1,32 @@
 """Python script for evaluating SLM output via GPT-4."""
 
+import os
+
+import openai
+
 import config
 
 
 def main():
     """Driver for script."""
+    openai.api_key = os.environ["OPENAI_API_KEY"]
+    client = openai.OpenAI(
+        organization="org-k4X6o1QPuBpEC8GJahNSdpo4",
+        project="proj_DUVloPcPSQnFty2IFRGocLkq",
+    )
+
+    response = client.chat.completions.create(
+        model = "gpt-4o",
+        messages = [
+            {
+                "role": "user",
+                "content": [{ "type": "text", "text": "Tell me a cool fact!"}]
+            }
+        ]
+    )
+
+    print(response.choices[0].message)
+
     return 0
 
 
