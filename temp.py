66eb1e75a30926e2ad1be88eced859a7989532b4
Parker Jensen <pjensen2718@gmail.com>
2024-11-11 12:21:10 -0500

    small work

--- temp.py
+++ temp.py
@@ -1,26 +1,49 @@
 import pathlib
+import sys
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import click
 
 
+def intro(model, dataset, all_official):
+    """Handle errors with options."""
+    if not model and not all_official:  # this can be changed depending on functionality for our own model
+        sys.exit("Error: a model must be specified or all official models must be specified.")
+    if model and all_official:
+        sys.exit("Error: cannot both specify specific model and use all official models.")
+
+
 @click.command()
-@click.option("-i", "--input_model", multiple=True, type=str, help="Select specific input model; can be used multiple times.")
-@click.option("-u", "--use_official", is_flag=True, help="Use official models; redundant if specific model requested.")
+@click.option("-m", "--model", multiple=True, type=str, help="Select specific model to evaluate; can be used multiple times.")
+@click.option("-d", "--dataset", type=str, help="Specify dataset.")
+@click.option("-a", "--all_official", is_flag=True, help="Use official models; redundant if specific model requested.")
+@click.option("-n", "--num_completions", default=10, help="Number of completions to be done by (each) model.")
 @click.option("-v", "--verbose", is_flag=True, help="Print verbose output.")
-def main(input_model, use_official, verbose):
-    if (input_model):  # input model specified
-        return -1 #todo
+def main(model, dataset, all_official, num_completions, verbose):
+    """Main driver for evaluation script."""
+    intro(model, dataset, all_official)  # option error handling
+
+    if (model):  # input model specified
+        model = AutoModelForCausalLM.from_pretrained(model)
+        # should we prompt for custom tokenizer?
+        tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-125M")  # page 2 of paper
+
     else:  # input model not specified
         # here, either we can use our own model or run through all official models; this can be done better with click later
         # TODO: potential functionality for our own model
+        if (all_official):
+            # official_models = [f"roneneldan/TinyStories-{n}M" for n in [1, 3, 8, 28, 33]]
+            official_models = ["roneneldan/TinyStories-1M"]
+            for model in official_models:
+                model = AutoModelForCausalLM.from_pretrained(model)
+                tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-125M")  # page 2 of paper
+                # is setting the EOS/pad token necessary?
+                # is model.eval() necessary?
+                
 
-        official_models = [f"roneneldan/TinyStories-{n}M" for n in [1, 3, 8, 28, 33]]
-        for model in official_models:
-            model = AutoModelForCausalLM.from_pretrained(model)
-            tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-125M")  # page 2 of paper
-
-        
+        else:  # use our own model?
+            print("ERROR: functionality for our own model not implemented.", file=sys.stderr)
+            return 1        
 
     return 0
 
