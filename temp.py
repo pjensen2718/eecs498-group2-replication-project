2b17ec1dcbb4f5596e10d753ebe32c22ef5ca935
Parker Jensen <pjensen2718@gmail.com>
2024-11-12 15:35:11 -0500

    more work on scalability

--- temp.py
+++ temp.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import re
 import sys
 import time
+# from threading import Thread, RLock  # RLock to allow with statement
 
 import click
 from openai import OpenAI
@@ -27,9 +28,9 @@ def get_prompts(dataset) -> list[str]:
     ]
 
 
-def intro(model: str, dataset: str, all_official: bool) -> list[str]:
+def intro(models: tuple[str], dataset: str, all_official: bool) -> list[str]:
     """Handle errors with options."""
-    if model and all_official:
+    if models and all_official:
         sys.exit("Error: cannot both specify specific model and use all official models.")
     if dataset:
         sys.exit("Error: functionality for user-defined dataset not implemented.")
@@ -37,7 +38,7 @@ def intro(model: str, dataset: str, all_official: bool) -> list[str]:
         return get_prompts(dataset)  # currently just returns a hard-coded list of strings
 
 
-def generate_completion(model, tokenizer, prompt: str, max_new_tokens: int = 300, top_k: int = 50) -> str:
+def gen_completion(model, tokenizer, prompt: str, max_new_tokens: int = 300, top_k: int = 50) -> str:
     """Generate an output based on a prompt from the model to be evaluated."""
     inputs = tokenizer(prompt, return_tensors="pt", padding=True)
     attention_mask = inputs['attention_mask']
@@ -54,12 +55,13 @@ def generate_completion(model, tokenizer, prompt: str, max_new_tokens: int = 300
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
 
 
-def generate_all_completions(model, tokenizer, prompts: list[str], output_per_prompt: int) -> dict[tuple[int, str], list]:
+def gen_all_completions(model, tokenizer, prompts: list[str],
+                        num_completions: int) -> dict[tuple[int,str], list]:
     """Generate all completions from given prompts."""
     outputs = collections.defaultdict(list)
     for i, prompt in enumerate(prompts):
-        for _ in range(output_per_prompt):
-            completion = generate_completion(model, tokenizer, prompt).split(prompt)
+        for _ in range(num_completions):
+            completion = gen_completion(model, tokenizer, prompt).split(prompt)
             outputs[(i, prompt)].append(prompt + "***" + completion[1])
     return outputs
 
@@ -67,9 +69,11 @@ def generate_all_completions(model, tokenizer, prompts: list[str], output_per_pr
 def construct_prompt(story_completion: str) -> tuple[str, str]:
     """Generate prompt to be forwarded to GPT-4o."""
     user_prompt_1 = f"""
-    Your task is to evaluate the performance of a student. The student is given the following exercise.
-    In the following exercise, the student is given a beginning of a story. The student needs to complete it into a full story.
-    The exercise tests the student's language abilities and creativity. The symbol *** marks the separator between the prescribed beginning and the student's completion:
+    Your task is to evaluate the performance of a student. The student is 
+    given the following exercise. In the following exercise, the student is 
+    given a beginning of a story. The student needs to complete it into a full 
+    story. The exercise tests the student's language abilities and creativity. 
+    The symbol *** marks the separator between the prescribed beginning and the student's completion:
     
     The student wrote the following story:
 
@@ -126,25 +130,49 @@ def grade_completion_with_gpt(story_completion: str) -> tuple[str, str]:
 
 
 # prompts may not be a list of strings in the end due to memory concerns, maybe a pandas dataframe or np array?
-def write_to_csv(model, tokenizer, prompts: list[str], num_completions: int, out_csv, err_csv) -> None:
-    headers = ["prompt_id", "prompt", "completion", "analysis", "grammar", "creativity", "consistency", "plot", "age_group"]
-    # headers = ["Prompt ID", "Prompt", "Completion", "Analysis", "Grammar", "Creativity", "Consistency", "Plot", "Age Group"]
-    # csv_writer.writerow(headers)
-    
-    # completions_by_prompt = generate_all_completions(prompts[0:1])
-    
-    # for (prompt_id, prompt), completions in completions_by_prompt.items():
-    #     for completion in completions:
-    #         user_prompt_1, user_prompt_2, story_completion = construct_prompt(completion)
-    #         analysis, grading = grade_completion_with_gpt(user_prompt_1, user_prompt_2)
-            
-    #         scores = re.findall(r"(\d+)/10", grading)
-    #         if len(scores) == 4:
-    #             scores = [float(score) for score in scores]
+def write_to_csv(model, tokenizer, prompts: list[str],
+                 num_completions: int, out_csv, err_csv) -> None:
+    """Write results of evaluation to two files: out and err (for errors)."""
+    # headers = ["Prompt ID", "Prompt", "Completion", "Analysis", "Grammar",
+    #            "Creativity", "Consistency", "Plot", "Age Group"]
+    # Write headers to both CSV files.
+    out_headers = ["prompt_id", "prompt", "completion", "analysis", "grammar",
+               "creativity", "consistency", "plot", "age_group"]
+    out_csv.writerow(out_headers)
+    err_headers = ["prompt_id", "prompt", "completion", "analysis", "grading",
+                   "reason_for_error"]
+    err_csv.writerow(err_headers)
+
+    # TODO: could probably be made faster with threading
+    completions_by_prompt = gen_all_completions(model, tokenizer,
+                                                prompts, num_completions)
+
+    for (prompt_id, prompt), completions in completions_by_prompt.items():
+        for completion in completions:
+            analysis, grading = grade_completion_with_gpt(completion)
+
+            scores = re.findall(r"(\d+)/10", grading)
+            if len(scores) == 4:  # four scores given
+                try:  # try to convert scores from strings to floats
+                    scores = [float(score) for score in scores]
+                except ValueError as e:
+                    # Score not convertible to float, i.e. invalid score.
+                    err_csv.write(prompt_id, prompt, completion, analysis,
+                                  grading, "invalid score")
+                    continue
+                if any([score > 10 or score < 0 for score in scores]):
+                    # Invalid score
+                    err_csv.write(prompt_id, prompt, completion, analysis,
+                                  grading, "score not between 0 and 10")
+                    continue
                 
-    #             if any(score > 10 for score in scores):
-    #                 print("Assigned score greater than 10.")
-    #                 continue
+                grammar, creativity, consistency, plot = scores
+                age_group = grading.split(':')[-1][1:]
+
+            else:  # four scores not given; erroneous output
+                err_csv.write(prompt_id, prompt, completion, analysis,
+                              grading, "four scores not given")
+
                 
     #             grammar, creativity, consistency, plot = scores
     #             age_group = grading.split(":")[-1][1:]
@@ -170,6 +198,7 @@ def write_to_csv(model, tokenizer, prompts: list[str], num_completions: int, out
 # prompts may not be a list of strings in the end due to memory concerns, maybe a pandas dataframe or np array?
 def evaluate_model(model_str: str, prompts: list[str], num_completions: int, verbose: bool) -> None:
     """Evaluate a SLM - combines all parts."""
+    print(model_str)
     model = AutoModelForCausalLM.from_pretrained(model_str)
     # should we prompt for custom tokenizer?
     tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-125M")  # page 2 of paper
@@ -179,6 +208,7 @@ def evaluate_model(model_str: str, prompts: list[str], num_completions: int, ver
 
     model.eval()  # put the model into evaluation mode
 
+    # Get prompts to generate completions.
     prompts = get_prompts(False)  # this currently just takes from a list of predefined prompts
 
     # NOTE: here we can either append to a previous CSV or create a new one; both without using unnecessary GPT prompts
@@ -194,25 +224,28 @@ def evaluate_model(model_str: str, prompts: list[str], num_completions: int, ver
     err_fname = f"err--{model_str.split('/')[1]}-{time_str}"
 
     eval_dir = Path("evals")
-    with open(eval_dir/"outs"/out_fname, "+w", newline="", encoding="utd-8") as out_csv:
+    with open(eval_dir/"outs"/out_fname, "+w", newline="", encoding="utf-8") as out_csv:
+        out_csv = csv.writer(out_csv, delimiter='|')  # non-comma delimiter
         with open(eval_dir/"errs"/err_fname, "+w", newline="", encoding="utf-8") as err_csv:
+            err_csv = csv.writer(err_csv, delimiter='|')  # non-comma delimiter
             write_to_csv(model, tokenizer, prompts, num_completions, out_csv, err_csv)
 
 
 @click.command()
-@click.option("-m", "--model", multiple=True, type=str, help="Select specific model to evaluate; can be used multiple times.")
+@click.option("-m", "--models", multiple=True, type=str, help="Select specific model to evaluate; can be used multiple times.")
 @click.option("-d", "--dataset", type=str, help="Specify dataset.")
 @click.option("-a", "--all_official", is_flag=True, help="Use all official models; redundant if specific model requested.")
 @click.option("-n", "--num_completions", default=10, help="Number of completions to be done by (each) model.")
 @click.option("-v", "--verbose", is_flag=True, help="Print verbose output.")
-def main(model, dataset, all_official, num_completions, verbose):
+def main(models, dataset, all_official, num_completions, verbose):
     """Main driver for evaluation script."""
-    prompts = intro(model, dataset, all_official)  # option error handling
+    prompts = intro(models, dataset, all_official)  # option error handling
 
     # currently, use this as "python3 temp.py -m roneneldan/TinyStories-1M"
 
-    if (model):  # input model specified
-        evaluate_model(model, prompts, num_completions, verbose)
+    if (models):  # input model specified
+        for model_str in models:  # since option can be used multiple times
+            evaluate_model(model_str, prompts, num_completions, verbose)
 
     else:  # input model not specified
         # here, either we can use our own model or run through all official models; this can be done better with click later
@@ -220,11 +253,11 @@ def main(model, dataset, all_official, num_completions, verbose):
         if (all_official):
             if __debug__: sys.exit("debug: do not try to evaluate all models")
             official_models = [f"roneneldan/TinyStories-{n}M" for n in [1, 3, 8, 28, 33]]
-            for model in official_models:
-                evaluate_model(model, prompts, num_completions, verbose)
+            for model_str in official_models:
+                evaluate_model(model_str, prompts, num_completions, verbose)
 
         else:  # use our own model?
-            sys.exit("ERROR: functionality for our own model not implemented.") 
+            sys.exit("Error: functionality for our own model not implemented.") 
 
     return 0
 
--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 """Python script for testing functionalities."""
 
-import evaluator
+from temp import construct_prompt
 
 
 def main():
     """Driver for script."""
+    user_prompt_1, user_prompt_2 = construct_prompt("Hello, world! Hello, world! Hello, world! Hello, world! Hello, world! Hello, world!")
+    print(user_prompt_1, "\n\n\n\n\n\n", user_prompt_2)
+    
     return 0
 
 
