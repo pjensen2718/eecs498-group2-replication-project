b18969934424db62411d4361d70232cb088c08a6
ericYuan17 <ericyuan@umich.edu>
2024-12-01 22:00:39 -0500

    added calculation for maximum rouge-2 score between all generated completions

--- rouge_k.py
+++ rouge_k.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from datasets import load_dataset
 
+# Split a string into a list of k-grams
 def get_k_grams(text, k):
     words = text.split()
     k_grams = []
@@ -16,6 +17,7 @@ def get_k_grams(text, k):
 
     return k_grams  
 
+# Formula for the Rouge-k precision score, calculated as the 
 def rouge_k_precision_score(generated_story, original_story, k):
     generated_kgrams = get_k_grams(generated_story, k)
 
@@ -35,14 +37,17 @@ def rouge_k_precision_score(generated_story, original_story, k):
 
     return round(score, 3)
 
+# Fetch the first 50 stories from the validation split of the dataset, whose beginnings have been used as our test prompts
 def get_validation_set():
     dataset = load_dataset("roneneldan/TinyStories")
-    return dataset['validation'][0:50]["text"]
+    return dataset['validation'][0:100]["text"]
 
+# Fetch the generated story completions 
 def get_unique_completions(outs_file):
     outs_df = pd.read_csv(outs_file, delimiter='|')
     return list(zip(outs_df['prompt_id'], outs_df['prompt'], outs_df['completion']))[::4]
 
+# Standardization of the format used for our completion file output for easier text processing
 def standardize(story, prompt):
     return story.replace("\n", "").replace("\\n", "").replace(prompt, "")
 
@@ -62,131 +67,85 @@ def similarity_to_original(outs_file):
     
     return scores
 
-# ////////////////////////////////////////////////////////////////////////////////
+# Uniqueness Section 2 
+def similarity_to_generated(outs_file):
+    unique_completions = get_unique_completions(outs_file) # prompt_id, prompt, completion
+    scores = []
+
+    for i, (prompt_id, prompt, completion) in enumerate(unique_completions):
+        generated_focus = standardize(completion, prompt)
+        max_score = 0
+        
+        for j, (other_prompt_id, other_prompt, other_completion) in enumerate(unique_completions):
+
+            if j != i:
+                generated_other = standardize(other_completion, other_prompt)
+                max_score = max(max_score, rouge_k_precision_score(generated_focus, generated_other, 2))
+
+        scores.append(max_score)
+
+    return scores
+
+
+# Uniqueness Section 3 
 
 # ////////////////////////////////////////////////////////////////////////////////
 
-outs_file_1M = "evals/outs/out--TinyStories-1M--2024-11-16--17-04-55.csv"
-outs_file_33M = "evals/outs/out--TinyStories-33M--2024-11-16--17-21-08.csv"
-
-scores_1M = similarity_to_original(outs_file_1M)
-scores_33M = similarity_to_original(outs_file_33M)
-
-mean_1M = sum(scores_1M)/len(scores_1M)
-mean_33M = sum(scores_33M)/len(scores_33M)
-
-import matplotlib.pyplot as plt
-
-fig, axes = plt.subplots(1, 2, figsize=(14, 6))
-
-axes[0].hist(scores_1M, bins=30, color='purple', edgecolor='black', alpha=0.7)
-axes[0].set_title('Rouge-2 Score For TinyStories-1M Model')
-axes[0].set_xlabel('Rouge-2 Score')
-axes[0].set_ylabel('Frequency')
-axes[0].text(0.2, 5, f'Mean: {mean_1M:.3f}', color='blue', fontsize=16, fontweight='bold')
-
-# Plot the histogram for scores_33M
-axes[1].hist(scores_33M, bins=30, color='purple', edgecolor='black', alpha=0.7)
-axes[1].set_title('Rouge-2 Score For TinyStories-33M Model')
-axes[1].set_xlabel('Rouge-2 Score')
-axes[1].set_ylabel('Frequency')
-axes[1].text(0.2, 5, f'Mean: {mean_33M:.3f}', color='blue', fontsize=16, fontweight='bold')
-
-plt.tight_layout()
-plt.show()
-
-# def rouge_k_fmeasure(original_story, generated_story, k):
-#     # Calculate precision in both directions
-#     precision_otg= rouge_k_precision_score(original_story, generated_story, k)
-#     precision_gto = rouge_k_precision_score(generated_story, original_story, k)
-
-#     # Calculate F-measure using harmonic mean
-#     if precision_otg + precision_gto > 0:
-#         fmeasure = (2 * precision_otg * precision_gto) / (precision_otg + precision_gto)
-#     else:
-#         fmeasure = 0.0
-
-#     return fmeasure
-
 # ////////////////////////////////////////////////////////////////////////////////
-#
-# ////////////////////////////////////////////////////////////////////////////////
+if __name__ == "__main__":
+    outs_file_1M = "evals/outs/out--TinyStories-1M--2024-11-16--17-04-55.csv"
+    outs_file_33M = "evals/outs/out--TinyStories-33M--2024-11-16--17-21-08.csv"
 
+    # Uniquness Section 1 (Similarity to original (sto)) **************************************************
+    sto_scores_1M = similarity_to_original(outs_file_1M)
+    sto_scores_33M = similarity_to_original(outs_file_33M)
 
-# outs_33M_run = "evals/outs/out--TinyStories-33M--2024-11-16--17-21-08.csv"
+    sto_mean_1M = sum(sto_scores_1M)/len(sto_scores_1M)
+    sto_mean_33M = sum(sto_scores_33M)/len(sto_scores_33M)
 
-# outputs_filename = 'evals/outs/out--TinyStories-1M--2024-11-12--23-46-15.csv'
+    fig, axes = plt.subplots(1, 2, figsize=(14, 6))
 
+    # Plot the histogram for sto_scores_1M_model
+    axes[0].hist(sto_scores_1M, bins=30, color='purple', edgecolor='black', alpha=0.7)
+    axes[0].set_title('Rouge-2 Score Distribution For TinyStories-1M Model')
+    axes[0].set_xlabel('Rouge-2 Score')
+    axes[0].set_ylabel('Frequency')
+    axes[0].text(0.2, 5, f'Mean: {sto_mean_1M:.3f}', color='blue', fontsize=16, fontweight='bold')
 
-# #original_stories_filename = 'file.csv'
+    # Plot the histogram for sto_scores_33M_model
+    axes[1].hist(sto_scores_33M, bins=30, color='purple', edgecolor='black', alpha=0.7)
+    axes[1].set_title('Rouge-2 Score Distribution For TinyStories-33M Model')
+    axes[1].set_xlabel('Rouge-2 Score')
+    axes[1].set_ylabel('Frequency')
+    axes[1].text(0.2, 5, f'Mean: {sto_mean_33M:.3f}', color='blue', fontsize=16, fontweight='bold')
 
-# df_outputs = pd.read_csv(outputs_filename, delimiter = '|')
+    plt.tight_layout() 
+    plt.show()
 
-# df_original = load_dataset("roneneldan/TinyStories")['train'].to_pandas()
+    # Uniquness Section 2 (Similarity to generated (stg))  **************************************************
+    stg_scores_1M = similarity_to_generated(outs_file_1M)
+    stg_scores_33M = similarity_to_generated(outs_file_33M)
 
-# print(df_original)
+    stg_mean_1M = sum(stg_scores_1M)/len(stg_scores_1M)
+    stg_mean_33M = sum(stg_scores_33M)/len(stg_scores_33M)
 
-# #df_original = pd.read_csv(original_stories_filename)
+    fig, axes = plt.subplots(1, 2, figsize=(14, 6))
 
-# print(df_outputs)
+    # Plot the histogram for sto_scores_1M_model
+    axes[0].hist(stg_scores_1M, bins=30, color='blue', edgecolor='black', alpha=0.7)
+    axes[0].set_title('Rouge-2 Score Distribution (OTHER) For TinyStories-1M Model')
+    axes[0].set_xlabel('Rouge-2 Score')
+    axes[0].set_ylabel('Frequency')
+    axes[0].text(0.15, 5, f'Mean: {stg_mean_1M:.3f}', color='blue', fontsize=16, fontweight='bold')
 
-# original_stories = df_original.iloc[:,0].tolist()
+    # Plot the histogram for sto_scores_33M_model
+    axes[1].hist(stg_scores_33M, bins=30, color='blue', edgecolor='black', alpha=0.7)
+    axes[1].set_title('Rouge-2 Score Distribution (OTHER) For TinyStories-33M Model')
+    axes[1].set_xlabel('Rouge-2 Score')
+    axes[1].set_ylabel('Frequency')
+    axes[1].text(0.15, 5, f'Mean: {stg_mean_33M:.3f}', color='blue', fontsize=16, fontweight='bold')
 
-# for i in range(len(df_outputs)):
-#     g_story = .replace("***", "")
-#     generated_stories[i] = g_story
+    plt.tight_layout() 
+    plt.show()
 
-
-# scores_generated_to_original = []
-# scores_generated_to_genereated = []
-# scores_generated_to_dataset = []
-# scores_generated_to_closest = []
-
-# # 
-# for index, row in df_outputs.iterrrows():
-#     # prompt_id|prompt|completion|analysis|grammar|creativity|consistency|plot|age_group
-#     print(row)
-
-#     values = row.split('|')
-    
-#     g_story = generated_stories[index]
-#     original_story = original_stories[values[0]]
-
-#     # 1. How much of the new generation is contained in the original story
-#     scores_generated_to_original.append(rouge_k_precision_score(g_story, original_story, 2))
-
-#     # 2. How similar the new generation is to the 99 other generated stories
-#     # max_score = 0
-#     # for i in generated_stories:
-#     #     gen_to_gen_score = rouge_k_fmeasure(g_story, i, 2)
-#     #     max_score = max(max_score, gen_to_gen_score)
-#     # scores_generated_to_genereated.append(max_score)
-
-#     # 3. Wo what extent the k-grams in the generated story copied from the training data
-
-
-#     # 4. How similar the generated story is to the closest point
-#     # max_score = 0
-#     # for i in original_stories:
-#     #     org_to_gen_score = rouge_k_fmeasure(i, g_story,2)
-#     #     max_Score = max(max_score, org_to_gen_score)
-#     # scores_generated_to_closest.append(max_score)
-
-
-# fig, axs = plt.subplots(1,4,figsize = (15,5))
-# labels = ['1', '2', '3', '4']
-
-# axs[0].bar(labels, scores_generated_to_original)
-# axs[0].set_title('Graph 1')
-
-# # axs[1].bar(labels, scores_generated_to_genereated)
-# # axs[1].set_title('Graph 2')
-
-# # axs[2].bar(labels, scores_generated_to_dataset)
-# # axs[2].set_title('Graph 3')
-
-# # axs[3].bar(labels, scores_generated_to_closest)
-# # axs[3].set_title('Graph 4')
-
-# plt.tight_layout
-# plt.show()
+    # Uniquness Section 3 (Similarity to dataaset (stData))  **************************************************]
